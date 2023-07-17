b58b902fdaf4e951a2da528a284fe0aaece266c1
aida <aida.denisa@gmail.com>
2023-07-17 18:04:14 +0300

    Update debug message

--- ghost/core/core/server/eventHandlers/comments.js
+++ ghost/core/core/server/eventHandlers/comments.js
@@ -6,7 +6,7 @@ module.exports = async (io, socket) => {
   // listen to to changes in value from client
   const commentAdded = async (ids) => {
     const count = await commentsService.controller.count({ options: { ids: ids } })
-    debug(`[Websockets] received comment add event from client, count is now ${count.count}`);
+    debug(`[Websockets] received comment add event from client`);
     // emit change in count to all other listeners
     socket.broadcast.emit('comments:count', count);
   }
